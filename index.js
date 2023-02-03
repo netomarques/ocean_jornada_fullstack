370bd3aea493d09dbb6352ddbcaffa899cbe2f4a
netomarques <wilsonmarquesg12@gmail.com>
2023-02-03 15:00:10 -0400

    Criação do script start. Criação [POST] /item (create)

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const app = express();
 
+// O que vier no body da requisição, está em JSON
+app.use(express.json());
+
+// Endpoint / -> Hello World
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("<h1>Hello World</h1>");
+});
+
+// Endpoint /oi -> Olá, mundo!
+app.get("/oi", function (req, res) {
+  res.send("Olá, mundo!");
+});
+
+// Lista de informações
+const itens = ["Gabigol", "Pedro", "Bruno Henrique"];
+//              0        1      2
+
+// CRUD -> Lista de informações
+
+// Endpoint Read All -> [GET] /item
+app.get("/item", function (req, res) {
+  res.send(itens);
+});
+
+// Endpoint Read Single by ID -> [GET] /item/:id
+app.get("/item/:id", function (req, res) {
+  const id = req.params.id;
+  const item = itens[id-1];
+  res.send(item);
+});
+
+// Endpoint Create -> [POST] /item
+app.post("/item", function (req, res) {
+  const item = req.body;
+  itens.push(item.nome);
+  res.send("Item criado com sucesso");
 });
 
 app.listen(3000);
