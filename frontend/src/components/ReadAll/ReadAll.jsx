684e18807b00d21c1fda712b1a823b30d20fecb1
netomarques <wilsonmarquesg12@gmail.com>
2023-02-24 10:14:07 -0400

    URL do backend atualizada

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -60,7 +60,8 @@ function ReadAll() {
 
     // Realizar requisição para backend obtendo a lista de itens
     async function realizarRequisicao() {
-        const url = "http://localhost:3000/item";
+        //const url = "http://localhost:3000/item";
+        const url = "https://ocean-jornada-fullstack-3fxl.onrender.com/item";
         const response = await fetch(url);
         const data = await response.json();
 
